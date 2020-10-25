ccece1ef5e036c517021a0177bf023eb6c5769d5
VibhorJaiswal <vibhor.jaiswal9.9@gmail.com>
2020-10-25 18:37:20 +0530

    phla commit

---
+++ script2.js
@@ -0,0 +1,25 @@
+console.log("connected");
+
+
+    if (window.innerWidth < 800) {
+        let temp = document.querySelectorAll('#card');
+        for (let i = 0; i < 4; i++) {
+            temp[i].setAttribute("class", "row card");
+            temp[i].id = "";
+        }
+    }
+
+
+// materialize
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, { duration: 300 });
+});
+
+
+
+$(document).ready(function(){
+    $(".dropdown-trigger").dropdown();
+  });
+
