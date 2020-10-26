a4e7cef6e7d7dda63ac2d597fbf0ef9a1f228389
VibhorJaiswal <69683278+VibhorJaiswal@users.noreply.github.com>
2020-10-26 18:46:18 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -29,3 +29,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems, {dismissible: false});
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {responsiveThreshold	: 0});
+  });
