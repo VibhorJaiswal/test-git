64cc9669a9e2d74a71926a9de2d568e9c349845b
SaltyBuckets <49991711+SaltyBuckets@users.noreply.github.com>
2020-10-27 11:56:26 +0530

    cleanup

--- firebase-auth.js
+++ firebase-auth.js
@@ -3,16 +3,9 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-
       console.log(authResult);
       return false;
     },
-    // uiShown: function () {
-    //   document.getElementById('loader').style.display = 'none';
-    // },
   },
   signInFlow: 'popup',
   // signInSuccessUrl: '<url-to-redirect-to-on-success>',
@@ -21,6 +14,4 @@ var uiConfig = {
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
   ],
 };
-// The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
-// ui.start('#firebaseui-auth-container2', uiConfig);
